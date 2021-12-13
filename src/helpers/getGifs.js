84486d34bb4660bcfc323278fa27d325546f7b1c
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-12-13 11:24:24 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=an1HnV9BfUg9VMugTC4xbQz98EjNbpYp`
+    const respuesta = await fetch( url );
+    const { data } = await respuesta.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+
+        }
+    })
+
+    return gifs;
+
+}
